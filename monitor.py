4d9700a0260057efae791dfb5d95bfc0adaf5652
69ares <35406032+69ares@users.noreply.github.com>
2023-01-22 21:32:12 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -3,6 +3,8 @@ import requests
 import json
 import time
 import ipaddress
+import tkinter as tk
+from tkinter import messagebox, simpledialog
 
 info = subprocess.STARTUPINFO()
 info.dwFlags = subprocess.STARTF_USESHOWWINDOW
@@ -11,47 +13,88 @@ info.wShowWindow = subprocess.SW_HIDE
 first_ips = set()
 ip_bad = set()
 ip_trust = set()
+bad_ip_trust = set()
 
-API_KEY = "API"
-THRESHOLD = 70
+root = tk.Tk()
+root.withdraw()
+API_KEY = "d70a3e867f6cc6b9f79b504270412b543fabed8f87d9a760d614a05205d73fad16c0e4072b9441bc"
+#API_KEY = simpledialog.askstring("API KEY", "Inserisci la tua API KEY:", parent=root)
+THRESHOLD = simpledialog.askinteger("THRESHOLD", "Inserisci il valore del THRESHOLD:", parent=root)
 
-def check_ip(ip):
+def check_ip(ip, process, pid):
     try:
         ip_address = ipaddress.ip_address(ip.split(':')[0])
         if ip_address.is_private:
             return
-        url = f"https://api.abuseipdb.com/api/v2/check?ip={ip.split(':')[0]}&key={API_KEY}"
-        response = requests.get(url)
-        data = json.loads(response.text)
-        if data.get("abuseConfidenceScore"):
-            if data["abuseConfidenceScore"] >= THRESHOLD:
-                with open("ip_bad.txt", "a") as f:
-                    f.write(ip.split(':')[0] + "\n")
-                    ip_bad.add(ip)
+        url = f"https://api.abuseipdb.com/api/v2/check?ipAddress={ip_address}&maxAgeInDays=90&verbose&key={API_KEY}"
+        info = subprocess.STARTUPINFO()
+        info.dwFlags = subprocess.STARTF_USESHOWWINDOW
+        info.wShowWindow = subprocess.SW_HIDE
+        response = subprocess.run(["curl", "-G", url, "-H", f"Key: {API_KEY}", "-H", "Accept: application/json"], stdout=subprocess.PIPE, startupinfo=info)
+
+        try:
+            data = json.loads(response.stdout.decode())
+        except json.decoder.JSONDecodeError:
+            print("Error decoding JSON response")
+            return
+        score = data["data"]["abuseConfidenceScore"]
+        if score >= THRESHOLD:
+            with open("bad_ip_trust.txt", "r") as f:
+                bad_ip_list = f.read().splitlines()
+#bad_ip_list = f.read().splitlines() DA QUESTO RISULTATO:
+#['103.162.20.168:System:0:2023-01-22 21:23:21']
+#bad_ip_list deve contenere solamente l'ip , il primo numero prima dei due punti :
+                print(bad_ip_list)
+            if ip.split(':')[0] in bad_ip_list:
+                print(f"IP {ip.split(':')[0]} already present in bad_ip_trust.txt, skipping termination process")
             else:
-                with open("ip_trust.txt", "a") as f:
-                    f.write(ip.split(':')[0] + "\n")
-                    ip_trust.add(ip)
+                with open("ip_bad.txt", "a") as f:
+                    f.write(ip.split(':')[0] + ':' + process + ':' + pid + "\n")
+                    ip_bad.add(ip)
+                    result = messagebox.askquestion("Terminate process", f"Do you want to terminate the process with PID {pid} il processo è {process}. Score abuse: {score}", icon='warning')
+                    if result == 'no':
+                        from datetime import datetime
+                        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        with open("bad_ip_trust.txt", "w") as f:
+                            f.write(ip.split(':')[0] + ':' + process + ':' + pid + ':' + current_time + "\n")
+                    elif result == 'yes':
+                        subprocess.run(["taskkill", "/PID", pid, '/F'], stdout=subprocess.PIPE)
+                        with open("ip_bad.txt", "a") as f:
+                            f.write(ip.split(':')[0] + ':' + process + ':' + pid + ':' + 'terminato' + "\n")
+                            print(f"Process with PID {pid} and IP {ip.split(':')[0]} has been terminated and added to ip_bad.txt with status terminato")
+                    else:
+                        print("Invalid input, process not terminated")
         else:
             with open("ip_trust.txt", "a") as f:
                 f.write(ip.split(':')[0] + "\n")
+                ip_trust.add(ip)
     except ValueError:
         return
 
+
+
 while True:
     # Get the list of IPs from the active connections
-    result = subprocess.run(["netstat", "-n"], startupinfo=info, stdout=subprocess.PIPE)
-    output = result.stdout.decode()
-    print(output)
+    result = subprocess.run(["netstat", "-ano"], startupinfo=info, stdout=subprocess.PIPE)
+    output = result.stdout.decode(errors='ignore',encoding='cp437')
     for line in output.splitlines():
         if "ESTABLISHED" in line:
             ip = line.split()[2]
             if ip not in first_ips:
                 first_ips.add(ip)
-                check_ip(ip)
-                print(ip)
+                for line in output.splitlines():
+                    if ip in line:
+                        pid = line.split()[-1]
+                        process_result = subprocess.run(["tasklist", "/FI", f"PID eq {pid}"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True)
+                        process_output = process_result.stdout.decode(errors='ignore',encoding='cp850')
+                        try:
+                            process = process_output.splitlines()[3].split()[0]
+                        except IndexError:
+                            process = "Unknown"
+                        print(process)
+                        check_ip(ip,process,pid)
     # Write the current IPs to the file
     with open("ip.txt", "w") as f:
         for ip in first_ips:
             f.write(ip + "\n")
-    time.sleep(10)
+    time.sleep(1)
